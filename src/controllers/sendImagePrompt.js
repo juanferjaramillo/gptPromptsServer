ffeab2ad3d42059af04850e7c06b2081c88b0adb
Juanfer Jaramillo <juanfer.jaramillo@gmail.com>
2025-05-24 16:46:06 -0500

    images online

--- src/controllers/sendImagePrompt.js
+++ src/controllers/sendImagePrompt.js
@@ -2,6 +2,8 @@ async function sendImagePrompt(req, res) {
 const fs = require('fs');
 const OpenAI = require("openai");
 const { toFile } = require("openai");
+const streamifier = require("streamifier");
+const axios = require("axios");
 
 const { imageUrl1, imageUrl2, prompt } = req.body;
 const { SKEY } = process.env;
@@ -9,6 +11,7 @@ const client = new OpenAI({
   apiKey: SKEY, 
 });
 
+/*
 const imageFiles = [
     "/home/juanfer/Sthemma/gptText/gptPromptsServer/src/Assets/photo1.jpg",
     "/home/juanfer/Sthemma/gptText/gptPromptsServer/src/Assets/photo2.jpg",
@@ -20,6 +23,23 @@ const images = await Promise.all(
             type: "image/jpeg",
         })
     ),
+);
+*/
+
+const imageUrls = [
+ imageUrl1,
+  imageUrl2
+];
+
+const images = await Promise.all(
+  imageUrls.map(async (url) => {
+    const response = await axios.get(url, { responseType: 'arraybuffer' });
+    const stream = streamifier.createReadStream(response.data);
+
+    return await toFile(stream, null, {
+      type: "image/jpeg",
+    });
+  })
 );
 
 console.log("sending request to OpenAI");
@@ -33,7 +53,7 @@ console.log("Response received");
 // Save the image to a file
 const image_base64 = rsp.data[0].b64_json;
 const image_bytes = Buffer.from(image_base64, "base64");
-fs.writeFileSync("/home/juanfer/Sthemma/gptText/gptPromptsServer/src/Assets/result.png", image_bytes);
+fs.writeFileSync("/home/juanfer/Sthemma/gptPrompts/gptPromptsServer/src/Assets/result.png", image_bytes);
 res.status(200).send("Image is ready!");
 }
 
